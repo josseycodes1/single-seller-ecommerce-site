5842883170de5f5fce9d714a766fd26f1b041b8b
josseycodes1 <adewumijosephine1@gmail.com>
2025-09-08 15:59:56 +0100

    seller

--- app/seller/login/page.js
+++ app/seller/login/page.js
@@ -170,7 +170,7 @@ export default function SellerLogin() {
               </div>
 
               <div className="text-sm">
-                <a href="/forgot-password" className="text-[#FC46AA] hover:text-[#F699CD]">
+                <a href="/seller/forgot-password" className="text-[#FC46AA] hover:text-[#F699CD]">
                   Forgot your password?
                 </a>
               </div>
