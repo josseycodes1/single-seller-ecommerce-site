fd6364cd1a105377e1061c3a443d010979f27695
josseycodes1 <adewumijosephine1@gmail.com>
2025-09-19 12:15:33 +0100

    ui banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -57,7 +57,7 @@ const Banner = () => {
       {/* Left image */}
       {banner?.secondary_image && (
         <div className="hidden md:flex items-center justify-center w-1/4">
-          <div className="relative w-48 h-48 rounded-full overflow-hidden border-4 border-white  shadow-lg">
+          <div className="relative w-48 h-48 overflow-hidden shadow-lg">
             <Image
               src={banner.secondary_image}
               alt="Banner secondary image"
