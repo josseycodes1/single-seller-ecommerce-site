176ae836445a17594d3ed2fdb78f743deb0a915d
josseycodes1 <adewumijosephine1@gmail.com>
2025-09-19 01:05:55 +0100

    navbar fix

--- components/seller/Navbar.jsx
+++ components/seller/Navbar.jsx
@@ -11,9 +11,9 @@ const Navbar = () => {
   return (
     <div className='flex items-center px-4 md:px-8 py-3 justify-between border-b'>
       <h1 onClick={()=>router.push('/')} className='w-28 lg:w-32 cursor-pointer text-josseypink2'> JOSSEYCART </h1>
-      <Link href="/all-products" target="_blank" rel="noopener noreferrer">
+      <Link href="/" target="_blank" rel="noopener noreferrer">
         <button className='bg-josseypink2 text-white px-5 py-2 sm:px-7 sm:py-2 rounded text-xs sm:text-sm'>
-          View Website
+          Home Page
         </button>
       </Link>
     </div>
