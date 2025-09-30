2aae40610eadcfb2bb9a6a902957a3f8bf793ccd
josseycodes1 <adewumijosephine1@gmail.com>
2025-09-30 15:24:20 +0100

    import sort

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,10 +1,11 @@
 "use client"
 import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
 import FeaturedProduct from "@/components/FeaturedProduct";
 import Footer from "@/components/Footer";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+
 
 const AboutUs = () => {
     const [products, setProducts] = useState([]);
