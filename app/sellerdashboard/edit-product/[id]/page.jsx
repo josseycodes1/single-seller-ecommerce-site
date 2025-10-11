91977d809138776dbeeb8fa232deae8a1053b9c5
josseycodes1 <adewumijosephine1@gmail.com>
2025-10-11 19:29:38 +0100

    button fix

--- app/sellerdashboard/edit-product/[id]/page.jsx
+++ app/sellerdashboard/edit-product/[id]/page.jsx
@@ -678,7 +678,7 @@ const EditProduct = () => {
                 <button
                   type="button"
                   onClick={addColor}
-                  className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 transition-colors"
+                  className="bg-josseypink2 text-white px-3 py-1 rounded hover:bg-blue-600 transition-colors"
                   disabled={loading}
                 >
                   Add
